e6f6bd210f749e257de5c91346ed785f7578029d
leeonky <leeonky@gmail.com>
2018-04-12 14:01:37 +0000

    Render search form

--- app/helpers/filter_set_helper.rb
+++ app/helpers/filter_set_helper.rb
@@ -2,4 +2,10 @@ module FilterSetHelper
   def test_render
     render 'filter_set/hello_world'
   end
+
+  def filter_set options={}, &block
+    content_tag :form, 'accept-charset': options[:'action-charset']||'UTF-8',
+      action: request.path, class: options[:class]||'filter-set', method: options[:method]||'get' do
+    end
+  end
 end
--- spec/helpers/filter_set_helper_spec.rb
+++ spec/helpers/filter_set_helper_spec.rb
@@ -3,9 +3,28 @@ require 'rails_helper'
 describe FilterSetHelper, type: :helper do
   it 'test render' do
     expect(helper.test_render).to eq_in_slim %Q(
-div
-  | hello world
+div hello world
     )
+  end
+
+  describe 'render search form' do
+    let(:request_path) { '/some-path' }
+
+    before do
+      allow(helper).to receive(:request).and_return double('request', path: request_path)
+    end
+
+    it 'render an empty form with default args' do
+      expect(helper.filter_set).to eq_in_slim %Q(
+form.filter-set action='#{request_path}' accept-charset='UTF-8' method='get'
+      )
+    end
+
+    it 'render with form args' do
+      expect(helper.filter_set 'action-charset': 'gbk', method: 'post', class: 'my-form').to eq_in_slim %Q(
+form.my-form action='#{request_path}' accept-charset='gbk' method='post'
+      )
+    end
   end
 end
 
