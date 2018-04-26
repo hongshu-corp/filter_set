864603700b32df3a9347d964e105e553d0c891a3
leeonky <leeonky@gmail.com>
2018-04-26 13:00:10 +0000

    Fix render overide issue

--- lib/filter_set/engine.rb
+++ lib/filter_set/engine.rb
@@ -7,7 +7,7 @@ module FilterSet
     end
 
     initializer "filter_set.init" do |app|
-      ActionController::Base.send :include, ::FilterSetConcern
+      ActionController::Base.send :prepend, ::FilterSetConcern
       app.config.assets.precompile += %w(filter_set/filter_set.css)
       app.config.i18n.load_path += Dir[Engine.root.join('config', 'locales','**', '*.{rb,yml}').to_s]
       app.config.i18n.default_locale = :"zh-CN"
